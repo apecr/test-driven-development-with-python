84750d9a8bf953269b82d33fb2521251b533c8f6
apecr <alberto.eyo@gmail.com>
2023-01-28 18:08:11 +0100

    Use LiveServerTestCase for funtional tests

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -1,19 +1,62 @@
 import time
 
-import pytest
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-@pytest.fixture(scope="session")
-def browser():
-    browser = webdriver.Firefox()
-    browser.get("http://localhost:8000")
+class NewVisitorTest(LiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+        self.browser.get(self.live_server_url)
 
-    yield browser
+    def tearDown(self) -> None:
+        self.browser.quit()
 
-    browser.quit()
+    def test_title_is_ok(self):
+        # She notices the page title and header mention to-do lists
+        assert 'To-Do' in self.browser.title
 
+    def test_assert_h1_correct_value(self):
+        # She notices the page title and header mention to-do lists
+        header_text = self.browser.find_element_by_tag_name('h1').text
+
+        assert 'To-Do' in header_text
+
+    def test_enter_a_to_do_list_straight_away(self):
+        # She is invited to enter a to-do item straight away
+        input_box = self.browser.find_element_by_id('id_new_item')
+
+        assert input_box.get_attribute('placeholder') == 'Enter a to-do item'
+
+    def test_create_first_to_do_list_element(self):
+        input_box = self.browser.find_element_by_id('id_new_item')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        assert self.__get_rows()[0].text == '1: Buy peacock feathers'
+
+    def test_create_two_to_do_list_elements(self):
+        input_box = self.browser.find_element_by_id('id_new_item')
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        input_box = self.browser.find_element_by_id('id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        assert self.__get_rows()[0].text == '1: Buy peacock feathers'
+        assert self.__get_rows()[1].text == '2: Use peacock feathers to make a fly'
+
+    def __get_rows(self):
+        table = self.browser.find_element_by_id('id_list_table')
+        return table.find_elements_by_tag_name('tr')
 
 # She is invited to enter a to-do item straight away
 
@@ -35,58 +78,3 @@ def browser():
 # She visits that URL - her to-do list is still there.
 
 # Satisfied, she goes back to sleep
-
-
-def test_title_is_ok(browser):
-    # She notices the page title and header mention to-do lists
-    assert 'To-Do' in browser.title
-
-
-def test_assert_h1_correct_value(browser):
-    # She notices the page title and header mention to-do lists
-    header_text = browser.find_element_by_tag_name('h1').text
-
-    assert 'To-Do' in header_text
-
-
-def test_enter_a_to_do_list_straight_away(browser):
-    # She is invited to enter a to-do item straight away
-    input_box = browser.find_element_by_id('id_new_item')
-
-    assert input_box.get_attribute('placeholder') == 'Enter a to-do item'
-
-
-def test_create_first_to_do_list_element(browser):
-    input_box = browser.find_element_by_id('id_new_item')
-    # She types "Buy peacock feathers" into a text box (Edith's hobby
-    # is tying fly-fishing lures)
-    input_box.send_keys('Buy peacock feathers')
-    input_box.send_keys(Keys.ENTER)
-    time.sleep(1)
-
-    check_for_row_in_list_table(browser, '1: Buy peacock feathers')
-
-
-def test_create_two_to_do_list_elements(browser):
-    input_box = browser.find_element_by_id('id_new_item')
-    # She types "Buy peacock feathers" into a text box (Edith's hobby
-    # is tying fly-fishing lures)
-    input_box.send_keys('Buy peacock feathers')
-    input_box.send_keys(Keys.ENTER)
-    time.sleep(1)
-    input_box = browser.find_element_by_id('id_new_item')
-    input_box.send_keys('Use peacock feathers to make a fly')
-    input_box.send_keys(Keys.ENTER)
-    time.sleep(1)
-
-    check_for_row_in_list_table(browser, '1: Buy peacock feathers', 1)
-    check_for_row_in_list_table(browser, '2: Use peacock feathers to make a fly', 2)
-
-    assert 1 == 2
-
-
-def check_for_row_in_list_table(browser, row_text, row_position):
-    table = browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-
-    assert rows[row_position].text == row_text
